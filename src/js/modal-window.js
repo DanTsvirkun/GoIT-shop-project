0da8da7b201bc84085be9b312fb833916dcabadc
Y.Tern <ternxstyle@gmail.com>
2020-06-25 09:39:56 +0300

    added logic modal script v.0.2

--- src/js/modal-window.js
+++ src/js/modal-window.js
@@ -1,15 +1,17 @@
 ////////////////////////////////////////////////////////////
-//LOGIC FOR MODAL WINDOW v.0.1//////////////////////////////
+//LOGIC FOR MODAL WINDOW v.0.2//////////////////////////////
 ////////////////////////////////////////////////////////////
 
 const btn = document.querySelector('#myBtn'); // Find your button id
 const modal = document.querySelector('#myModal'); // Find your modal window id (wrapper)
+// Example: <span id="cross" class="close">&times;</span>
 const close = document.querySelector('#cross'); // Find your close element (cross - "x")
 
 // Open modal window
 btn.addEventListener('click', () => {
   modal.style.display = 'flex';
 });
+
 // Close modal window with click on the cross - "x"
 close.addEventListener('click', () => {
   modal.style.display = 'none';
