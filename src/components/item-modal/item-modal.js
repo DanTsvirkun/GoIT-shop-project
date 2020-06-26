74ff15e2420b07aca1c258ac9b8f84934280a522
rynazavr <lunevaira@gmail.com>
2020-06-26 18:27:57 +0300

    save before pull

--- src/components/item-modal/item-modal.js
+++ src/components/item-modal/item-modal.js
@@ -7,12 +7,13 @@ const cat = document.querySelector('.categories');
 cat.insertAdjacentHTML('beforeend', itemModalCardTablet());
 
 //ADD TO FAVORITES
-const heartBtn = document.querySelector('.icon-fav');
-heartBtn.addEventListener('click', addToFavourites);
-function addToFavourites(e) {
-  heartBtn.classList.toggle('icon-fav--active');
-}
+// const heartBtn = document.querySelector('.icon-fav');
+// heartBtn.addEventListener('click', addToFavourites);
+// function addToFavourites(e) {
+//   heartBtn.classList.toggle('icon-fav--active');
+// }
 
+/////////
 //siema-slider-mobile
 class SiemaWithDots extends Siema {
   addDots() {
@@ -65,3 +66,35 @@ document.body.onclick = function (event) {
   }
 };
 ////
+const refsItMob = {
+  itemCode: document.querySelector('.item_modal--item-code').textContent,
+  itemName: document.querySelector('.item_modal--header').textContent,
+  itemPrice: document.querySelector('.item_modal--item-pricevalue').textContent,
+};
+
+console.log(refsItMob.itemName);
+console.log(refsItMob.itemCode);
+console.log(refsItMob.itemPrice);
+// let array = [];
+// localStorage.setItem('name', JSONstringify(array));
+
+///////////////////////////////////
+// localStorage.setItem('names', JSON.stringify(dataLsItem));
+// let favorites = JSON.parse(localStorage.getItem(dataLsItem));
+// favorites.forEach(function (favorite) {
+//   document.getElementById(favorite).className = 'fav';
+// });
+// document.querySelector('.icon-fav').addEventListener('click', function (e) {
+//   let id = e.target.id,
+//     item = e.target,
+//     index = favorites.indexOf(id);
+//   if (!id) return;
+//   if (index == -1) {
+//     favorites.push(id);
+//     item.className = 'fav';
+//   } else {
+//     favorites.splice(index, 1);
+//     item.className = '';
+//   }
+//   localStorage.setItem('favorites', JSON.stringify(favorites));
+// });
