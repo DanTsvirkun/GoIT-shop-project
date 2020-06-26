bdd514ec59cf1e2965eedf7ceeb3dbfac34c1354
DanTsvirkun <danila.tsvirkun555@gmail.com>
2020-06-26 14:06:49 +0300

    saving some things, just in case

--- src/components/header-main/js/header-main.js
+++ src/components/header-main/js/header-main.js
@@ -1,6 +1,46 @@
 import '../css/header-main.css';
 import refs from './refs.js';
 import authBlock from '../templates/auth-block.hbs';
+import categoriesList from '../templates/categories.hbs';
 
+let activeCategoryATM;
+
+const testArrayFromBack = [
+  'Недвижимость',
+  'Транспорт',
+  'Работа',
+  'Электроника',
+  'Бизнес и услуги',
+  'Отдых и спорт',
+  'Отдам бесплатно',
+  'Обмен',
+];
+
+const categoriesMarkup = categoriesList(testArrayFromBack);
+refs.categories.insertAdjacentHTML('beforeend', categoriesMarkup);
 const authBlockMarkup = authBlock();
+refs.authBlockMobile.insertAdjacentHTML('beforeend', authBlockMarkup);
 refs.authBlock.insertAdjacentHTML('beforeend', authBlockMarkup);
+refs.categories.addEventListener('click', activeCategory);
+refs.clearBlock.addEventListener('click', clearActiveCategory);
+
+function activeCategory(e) {
+  if (e.target !== e.currentTarget) {
+    if (document.querySelector('.active-category')) {
+      activeCategoryATM = document.querySelector('.active-category');
+      activeCategoryATM.classList.remove('active-category');
+    }
+    if (e.target.classList.contains('active-category')) {
+      return;
+    } else {
+      e.target.classList.add('active-category');
+    }
+  }
+}
+
+function clearActiveCategory() {
+  if (document.querySelector('.active-category')) {
+    activeCategoryATM = document.querySelector('.active-category');
+    activeCategoryATM.classList.remove('active-category');
+  }
+}
--- src/components/header-main/js/refs.js
+++ src/components/header-main/js/refs.js
@@ -1,3 +1,8 @@
 export default {
   authBlock: document.querySelector('.header-auth'),
+  test: document.querySelector('.test'),
+  categories: document.querySelector('.categories-filter'),
+  clearBlock: document.querySelector('.clear-filters-wrapper'),
+  mobileMenu: document.querySelector('.mobile-menu'),
+  authBlockMobile: document.querySelector('.header-auth-mobile'),
 };
