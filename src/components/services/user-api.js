eba36681cfdfd119edc42749c30e1f5395fcef64
Oscolok <oscolok7@gmail.com>
2020-06-29 14:30:36 +0300

    add api logic

---
+++ src/components/services/user-api.js
@@ -0,0 +1,99 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://identitytoolkit.googleapis.com/v1/accounts';
+const API_URL = 'https://api-project-575025675995.firebaseio.com';
+const API_KEY = 'AIzaSyBagS6Xzts8IVgIgwZ3ER5WfdgPqLtF_DA';
+
+export const getUserInfo = userId => {
+  return axios.get(`${API_URL}/user/${userId}.json`);
+};
+
+export const signInUser = signInUser => {
+  axios
+    .post(`:signInWithPassword?key=${API_KEY}`, {
+      ...signInUser,
+      returnSecureToken: true,
+    })
+    .then(res => {
+      if (res.status === 200) {
+        getUserInfo('').then(userArr => {
+          const foundUser = Object.values(userArr.data).find(
+            user => user.email === res.data.email,
+          );
+
+          localStorage.setItem(
+            'user-info',
+            JSON.stringify({
+              userId: foundUser.userId,
+              email: res.data.email,
+              // token: res.data.idToken,
+            }),
+          );
+        });
+      }
+    })
+    .catch(err => console.log(err));
+};
+
+export const signUpUser = signUpUser => {
+  const { firstName, secondName, email, phone, password } = signUpUser;
+
+  axios
+    .post(`:signUp?key=${API_KEY}`, {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    })
+    .then(res => {
+      if (res.status === 200) {
+        axios
+          .post(`${API_URL}/user.json`, {
+            firstName: firstName,
+            secondName: secondName,
+            email: email,
+            phone: phone,
+          })
+          .then(resId =>
+            localStorage.setItem(
+              'user-info',
+              JSON.stringify({
+                userId: resId.data.name,
+                email: res.data.email,
+                // token: res.data.idToken,
+              }),
+              axios.patch(`${API_URL}/user/${resId.data.name}.json`, {
+                userId: resId.data.name,
+              }),
+            ),
+          );
+        console.log('register');
+      }
+    })
+    .catch(err => console.log(err));
+};
+
+export const signOutUser = () => {
+  localStorage.clear();
+};
+
+export const updateUserAvatar = (id, obj) => {
+  return axios.patch(`${API_URL}/user/${id}.json`, { avatar: obj });
+};
+
+export const addUserAdv = (userId, advId) => {
+  return axios.patch(`${API_URL}/user/${userId}/adv.json`, { [advId]: 'key' });
+};
+
+export const addUserFavourite = (userId, advId) => {
+  return axios.patch(`${API_URL}/user/${userId}/favourite.json`, {
+    [advId]: 'key',
+  });
+};
+
+export const deleteUserFavourite = (userId, advId) => {
+  return axios.delete(`${API_URL}/user/${userId}/favourite/${advId}.json`);
+};
+
+// deleteData('-MAkWY0ZZG5Ji2ge1Ndu', '-MZZsadgagsai2ge1Ndd')
+//   .then(res => console.log('test', res))
+//   .catch(err => console.log(err));
--- src/index.js
+++ src/index.js
@@ -1,1 +1,104 @@
 import './css/main.css';
+import {
+  getUserInfo,
+  signInUser,
+  signUpUser,
+  signOutUser,
+  updateUserAvatar,
+  addUserAdv,
+  addUserFavourite,
+} from './components/services/user-api';
+
+const userInfoHtml = document.querySelector('.user-info');
+const resultIMG = document.querySelector('.resultIMG');
+const signInForm = document.querySelector('.auth-form-sign-in');
+const signUpForm = document.querySelector('.auth-form-sign-up');
+const signOutForm = document.querySelector('.auth-form-sign-out');
+
+signInForm.addEventListener('input', hendelInputSave);
+signInForm.addEventListener('submit', hendelSubmitSignIn);
+
+signUpForm.addEventListener('input', hendelInputSave);
+signUpForm.addEventListener('submit', hendelSubmitSignUp);
+
+signOutForm.addEventListener('click', hendelSignOut);
+
+const inputData = {
+  userSignIn: {},
+  userSignUp: {},
+};
+
+function hendelInputSave(e) {
+  inputData[e.currentTarget.name][e.target.name] = e.target.value;
+}
+
+function hendelSubmitSignIn(e) {
+  e.preventDefault();
+  signInUser(inputData.userSignIn);
+
+  testLog();
+}
+
+function hendelSubmitSignUp(e) {
+  e.preventDefault();
+  signUpUser(inputData.userSignUp);
+
+  testLog();
+}
+
+function hendelSignOut(e) {
+  signOutUser();
+
+  testLog();
+}
+
+function userInfo() {
+  if (localStorage.getItem('user-info')) {
+    const localUserId = JSON.parse(localStorage.getItem('user-info')).userId;
+    getUserInfo(localUserId).then(res => showUserInfo(res.data));
+  }
+}
+userInfo();
+
+function showUserInfo(obj) {
+  userInfoHtml.innerHTML = `Здравствуйте: ${obj.firstName} ${obj.secondName}. Эмейл: ${obj.email}. Телефон: ${obj.phone}`;
+  if (obj.avatar) {
+    resultIMG.src = obj.avatar;
+  }
+}
+
+testLog();
+function testLog() {
+  setTimeout(() => {
+    if (localStorage.getItem('user-info')) {
+      console.log('i am login');
+    } else {
+      console.log('i am not login');
+    }
+  }, 1000);
+}
+
+// AVATAR
+const fileForm = document.forms.fileForm;
+
+function toDataURL(element) {
+  return new Promise(resolve => {
+    const reader = new FileReader();
+
+    reader.onloadend = () => resolve(reader.result);
+    reader.readAsDataURL(element.files[0]);
+  });
+}
+
+const createbase = e => {
+  e.preventDefault();
+
+  const element = fileForm.elements.fileFormInput;
+
+  toDataURL(element).then(data => {
+    const localUserId = JSON.parse(localStorage.getItem('user-info')).userId;
+    updateUserAvatar(localUserId, data);
+  });
+};
+
+fileForm.addEventListener('submit', createbase);
