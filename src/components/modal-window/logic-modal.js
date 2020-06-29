1eacdd0566652c5a516610040acedef0f8d4b8e5
Y.Tern <ternxstyle@gmail.com>
2020-06-29 18:52:46 +0300

    added new version logic

--- src/components/modal-window/logic-modal.js
+++ src/components/modal-window/logic-modal.js
@@ -1,59 +1,51 @@
 ////////////////////////////////////////////////////////////
-//LOGIC FOR MODAL WINDOW v.0.6//////////////////////////////
+//LOGIC FOR MODAL WINDOW v.0.7//////////////////////////////
 ////////////////////////////////////////////////////////////
 
-export default class ModalLogic {
-  constructor({
-    modalid,
-    crossid,
-    openid,
-    markup
-  }) {
-    this.modal = document.querySelector(modalid); // Find your modal window
-    this.close = document.querySelector(crossid); // Find your close element (cross - "x")
-    this.btn = document.querySelector(openid); // Find your open button
-    this.markup = markup; // Your markup string
-    this.modalEventListeners();
+//////////////Вызвать метод modalBackdrop, передать разметку/////////////////////////////
+//<div class="modalContainer"><div> - этот див с классом должен быть в основной разметке,
+//так как на него повешены глобальные настройки
+/////////////////////////////////////////////////////////////////////////////////////////
+
+export const modalBackDrop = (innerElement) => {
+  const container = document.querySelector('.modalContainer');
+  const createModalMarkup = (closeModal) => {
+    return `
+    <div class ="modal">
+        ${innerElement};
+    </div>
+    `;
   }
-  modalEventListeners() {
-    this.btn.addEventListener('click', this.createMarkup);
-    this.close.addEventListener('click', this.closeModalWithCross);
-    window.addEventListener('click', closeModalWithArea);
-    window.addEventListener('keydown', closeModalWithEsc);
+  const closeModal = () => {
+    container.innerHTML = '';
+    container.addEventListener('click', close);
+    document.removeEventListener('keydown', close);
   }
-  createMarkup() {
-    this.modal.innerHTML = this.markup;
+
+  const close = (e) => {
+    if (e.target === document.querySelector('.modal') || e.key === 'Escape') {
+      closeModal();
+    } else return;
   }
-  closeModal() {
-    this.modal.innerHTML = '';
-    this.close.removeEventListener('click', closeModalWithCross);
-    window.removeEventListener('click', closeModalWithArea);
-    window.removeEventListener('keydown', closeModalWithEsc);
-  }
-  // Close modal window with click on the cross - "x"
-  closeModalWithCross() {
-    this.closeModal();
-  }
-  // Close modal window with click on the grey area
-  closeModalWithArea(e) {
-    if (e.target !== this.modal) {
-      this.closeModal();
-    }
-  }
-  // Close modal window with "Escape-button"
-  closeModalWithEsc(e) {
-    if (e.code === 'Escape') {
-      this.closeModal();
-    }
-  }
+
+  container.innerHTML = createModalMarkup(closeModal);
+  container.addEventListener('click', close);
+  document.addEventListener('keydown', close);
+  return closeModal;
 }
 
-/////////////////////////////////////////////////////////////////////////////
-/// Для того, чтобы скрипт сработал, нужно:                               ///
-///1. подставить названия ваших айдишников (id), (если их нет, то создать)///
-///2. функции импортировать в свой ".js-файл"                             ///
-///3. найти свой id, через который вы будете открывать модалку            ///
-///Создание элемента крестик ("cross"):                                   ///
-///<span id="cross">&times;</span>                                        ///
-///На всякий случай... Всем вдохновния при разработке! ;-)                ///
-/////////////////////////////////////////////////////////////////////////////
+////////Your element///////////////////////////////////////////////////////////////
+
+const adv = () => {
+  const innerElement2 = `
+    <div class="innerBlock2">
+    <h2>Open Window</h2>
+    <button class="closeBtnAdv">x</button>
+    </div>
+    `;
+  const closeModal = modalBackDrop(innerElement2); // !!!!!!!!!! Передать разметку, в ответ получить метод на закрытие окна
+  const closeBtn = document.querySelector('.closeBtnAdv');
+  closeBtn.addEventListener('click', closeModal);
+}
+
+adv();
--- src/index.js
+++ src/index.js
@@ -4,3 +4,5 @@ import './components/adv-block/adsSection';
 import './components/section-categories/categories';
 import './components/services/api';
 import './components/modal-window/logic-modal.js';
+import './components/modal-window/styles.css';
+
