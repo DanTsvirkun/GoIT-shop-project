6732d0f927c2c201b8a227866a40526decf6cc98
Andrii Kulyk <kyluk5@gmail.com>
2020-06-30 15:42:23 +0300

    show_real_info

--- src/components/adv-block/adsSection.js
+++ src/components/adv-block/adsSection.js
@@ -4,46 +4,76 @@ import Siema from 'siema';
 import adsTemplateSM from '../adv-block/templates/block__list/adsTemplateSM.hbs';
 import adsTemplateMD from '../adv-block/templates//block__list/adsTemplateMD.hbs';
 import adsTemplateArround from './templates/arround-block__list/arround-block__list.hbs';
-import adsTemplateArroundLG from './templates/arround-block__list/arround-block__list-LG.hbs';
+import { api } from '../services/api';
+
 // =========================================================
 const blockList = document.querySelector('.block__list');
 const arroundBlockList = document.querySelector('.arround-block__list');
+const horizontalBlock = document.querySelector('.horizontal-block');
 // =========================================================
 
+// =========================================================
 if (window.matchMedia('(max-width: 767px)').matches) {
-  // const object = data.hits.map(item => adsTemplateSM(item)).join('');
-  // blockList.insertAdjacentHTML('beforeend', object);
-  blockList.insertAdjacentHTML('beforeend', adsTemplateSM());
-  const mySiema = new Siema({
-    selector: blockList,
-    loop: true,
-    duration: 1000,
-  });
-  setInterval(() => {
-    mySiema.next();
-  }, 3500);
-} else if (
+  api
+    .getAllGoods()
+    .then(data => data.map(item => adsTemplateSM(item)))
+    .then(item => {
+      blockList.innerHTML = item;
+      const mySiema = new Siema({
+        selector: blockList,
+        loop: true,
+        duration: 1000,
+      });
+      setInterval(() => {
+        mySiema.next();
+      }, 3500);
+    });
+}
+// =========================================================
+else if (
   window.matchMedia('(min-width: 768px)' && '(max-width: 1279px)').matches
 ) {
-  blockList.insertAdjacentHTML('beforeend', adsTemplateMD());
-  const mySiema = new Siema({
-    selector: blockList,
-    loop: true,
-    duration: 1000,
-  });
-  setInterval(() => {
-    mySiema.next();
-  }, 3500);
-  arroundBlockList.insertAdjacentHTML('beforeend', adsTemplateArround());
-} else if (window.matchMedia('(min-width: 1280px)').matches) {
-  blockList.insertAdjacentHTML('beforeend', adsTemplateMD());
-  const mySiema = new Siema({
-    selector: blockList,
-    loop: true,
-    duration: 1000,
-  });
-  setInterval(() => {
-    mySiema.next();
-  }, 3500);
-  arroundBlockList.innerHTML = adsTemplateArroundLG();
+  api
+    .getAllGoods()
+    .then(data => data.map(item => adsTemplateMD(item)))
+    .then(item => {
+      blockList.innerHTML = item;
+      const mySiema = new Siema({
+        selector: blockList,
+        loop: true,
+        duration: 1000,
+      });
+      setInterval(() => {
+        mySiema.next();
+      }, 3500);
+    });
+  api
+    .getAllGoods()
+    .then(data => data.map(item => adsTemplateArround(item)).slice(0, 2))
+    .then(item => (arroundBlockList.innerHTML = item.join('')));
+}
+// =========================================================
+else if (window.matchMedia('(min-width: 1280px)').matches) {
+  api
+    .getAllGoods()
+    .then(data => data.map(item => adsTemplateMD(item)))
+    .then(item => {
+      blockList.innerHTML = item;
+      const mySiema = new Siema({
+        selector: blockList,
+        loop: true,
+        duration: 1000,
+      });
+      setInterval(() => {
+        mySiema.next();
+      }, 3500);
+    });
+  api
+    .getAllGoods()
+    .then(data => data.map(item => adsTemplateArround(item)).slice(0, 2))
+    .then(item => (arroundBlockList.innerHTML = item.join('')));
+  api
+    .getAllGoods()
+    .then(data => data.map(item => adsTemplateArround(item)).slice(0, 3))
+    .then(item => (horizontalBlock.innerHTML = item.join('')));
 }
