a9c68be7f11a5d9a65cbc6476ad4d4ad39755696
DanTsvirkun <danila.tsvirkun555@gmail.com>
2020-07-01 17:44:18 +0300

    commiting to fix each category

--- src/components/section-categories/categories.js
+++ src/components/section-categories/categories.js
@@ -123,6 +123,39 @@ function showMoreCategories(e) {
   }
   fnSwitch(counterStartIdx, counterEndIdx);
 }
+
+window.addEventListener('resize', () => {
+  console.log(111);
+  if (window.matchMedia('(max-width: 767px)').matches) {
+    const mySiema = new Siema({
+      selector: list,
+      loop: true,
+      duration: 1000,
+    });
+    setInterval(() => {
+      mySiema.next();
+    }, 4000);
+  } else if (
+    window.matchMedia('(min-width: 768px)' && '(max-width: 1279px)').matches
+  ) {
+    const mySiema = new Siema({
+      selector: list,
+      duration: 200,
+      perPage: 2,
+    });
+    slidePrev.addEventListener('click', () => mySiema.prev());
+    slideNext.addEventListener('click', () => mySiema.next());
+  } else if (window.matchMedia('(min-width: 1280px)').matches) {
+    const mySiema = new Siema({
+      selector: list,
+      duration: 200,
+      perPage: 4,
+    });
+    slidePrev.addEventListener('click', () => mySiema.prev());
+    slideNext.addEventListener('click', () => mySiema.next());
+  }
+});
+
 // ==============================================
 // import './categories-styles/category.css';
 // import './categories-styles/more-info.css';
