baefdbc6c7b1d370594843507659ed26c46a3d9b
Y.Tern <ternxstyle@gmail.com>
2020-07-02 11:07:17 +0300

    change code_3

--- src/components/modal-window/logic-modal.js
+++ src/components/modal-window/logic-modal.js
@@ -1,42 +1,44 @@
 ////////////////////////////////////////////////////////////
 //LOGIC FOR MODAL WINDOW v.0.7//////////////////////////////
 ////////////////////////////////////////////////////////////
-
+​
 //////////////Вызвать метод modalBackdrop, передать разметку/////////////////////////////
-//<div class="modalContainer"><div> - этот див с классом должен быть в основной разметке,
+//<div class="modalContainer"></div> - этот див с классом должен быть в основной разметке,
 //так как на него повешены глобальные настройки
-/////////////////////////////////////////////////////////////////////////////////////////
-
-export const modalBackDrop = (innerElement) => {
+/////////////////////////////////////////////////////////////////////////////////////////​
+export const modalBackDrop = innerElement => {
+  const body = document.querySelector('body');
   const container = document.querySelector('.modalContainer');
-  const createModalMarkup = (closeModal) => {
+  const createModalMarkup = closeModal => {
+    body.classList.add('lockOverflow');
     return `
-    <div class ="modal">
-        ${innerElement};
-    </div>
+        ${innerElement}
     `;
-  }
-  
+  };​
   const closeModal = () => {
-    container.innerHTML = '';
+    container.classList.remove('show-modal');
     container.addEventListener('click', close);
     document.removeEventListener('keydown', close);
-  }
-
-  const close = (e) => {
-    if (e.target === document.querySelector('.modal') || e.key === 'Escape') {
+    document.querySelector('body').style.overflow = 'unset';
+  };​
+  const close = e => {
+    if (
+      e.target === document.querySelector('.modalContainer') ||
+      e.key === 'Escape'
+    ) {
       closeModal();
     } else return;
-  }
-
+  };​
   container.innerHTML = createModalMarkup(closeModal);
+  container.classList.add('show-modal', 'transition-effect');
+  document.querySelector('body').style.overflow = 'hidden';
   container.addEventListener('click', close);
   document.addEventListener('keydown', close);
   return closeModal;
-}
+};​
 
 ////////Your element///////////////////////////////////////////////////////////////
-
+​
 /* const adv = () => {
   const innerElement2 = `
     <div class="innerBlock2">
@@ -48,6 +50,6 @@ export const modalBackDrop = (innerElement) => {
   const closeBtn = document.querySelector('.closeBtnAdv');
   closeBtn.addEventListener('click', closeModal);
 }
-
+​
 adv();
  */
