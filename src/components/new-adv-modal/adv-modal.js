a9fa8f3ba2f68c9f6300d26fef1728200f597740
Mypaxa4e <ivanshtypula@gmail.com>
2020-07-02 22:20:28 +0300

    adv modal tel deleted price on free hide

--- src/components/new-adv-modal/adv-modal.js
+++ src/components/new-adv-modal/adv-modal.js
@@ -1,10 +1,9 @@
-// import {api} from '../services/api'
+import {api} from '../services/api'
 import { modalBackDrop } from '../modal-window/logic-modal.js';
 import '../modal-window/styles.css';
 import './adv-styles.css';
 
-const refs = {
-  // button: document.querySelector('.header__create-ad-btn'),
+const refs = {  
   button: document.querySelectorAll('.modal-btn'),
 };
 
@@ -89,15 +88,10 @@ const markupModal = `
 
         <div class="input-wrapper input-wrapper__price">
           <p class="adv-modal__product-price-title">Цена</p>
-          <input type="text" name="productPrice" data-source="price" class="adv-modal__product-price input"
+          <input type="number" name="productPrice" data-source="price" class="adv-modal__product-price input"
             autocomplete="off" maxlength="16" placeholder="0.00 грн">
         </div>
-
-        <div class="input-wrapper">
-          <p class="adv-modal__product-telephone-number-title">Телефон</p>
-          <input type="tel" name="productPhone" data-source="phone" class="adv-modal__product-telephone-number input"
-            minlength="10" placeholder="+38 (0--) --- -- --" required>
-        </div>
+        
       </div>
       <button type="submit" class="add-product-btn" data-close="">Добавить</button>
 
@@ -124,36 +118,33 @@ function createModal() {
 
   imgLoaderArea.addEventListener('click', chooseImgBlock);
   imgLoaderArea.addEventListener('change', previewImg);
-  imgLoaderArea.addEventListener('change', changeImgBlock);
 }
 
 function saveData(event){ 
   const userInfo = JSON.parse(localStorage.getItem('user-info'));
   const productName = event.currentTarget.elements.productName;
   const productDescription = event.currentTarget.elements.productDescription;
-  const productPrice = event.currentTarget.elements.productPrice;
-  const productPhone = event.currentTarget.elements.productPhone;
+  const productPrice = event.currentTarget.elements.productPrice;  
   const productCategory = event.currentTarget.elements.productCategory;
+ 
 
   createData = {
-   author: userInfo,
+   author: userInfo.userId,
    name: productName.value,
    mainImg: '',
    image: [],
    category: productCategory.value === 'category'? '' : productCategory.value,
    description: productDescription.value,
-   price: productPrice.value,
-   phone: productPhone.value,
+   price: productPrice.value,  
  }
 
   const productPriceWrap = document.querySelector('.input-wrapper__price');  
 
  event.target.value === 'for-free' ? productPriceWrap.classList.add('hide-price') : productPriceWrap.classList.remove('hide-price');
  event.target.value === 'for-free' ? productPriceWrap.classList.remove('input-wrapper') : productPriceWrap.classList.add('input-wrapper'); 
-}
+}  
 
-// let promise;
-
+//==================================
 function chooseImgBlock(event) {
   if (event.target === event.currentTarget) {
     return;
@@ -163,13 +154,9 @@ function chooseImgBlock(event) {
   }
   
   const imgTarget = event.target;
-  imgTarget.setAttribute('type', 'file');
-
-  // console.dir(imgTarget.nextElementSibling.firstChild); 
-
-  
+  imgTarget.setAttribute('type', 'file'); 
 }
-
+//====================================
 function submitForm(event){
   event.preventDefault();
   if (createData.category === '') {
@@ -213,42 +200,64 @@ function submitForm(event){
 
     arr.filter(item => item.dataset.active)
     .map(item => item.dataset.active = "");
-
     arr[0].dataset.active = true;
-  }
-    console.log(createData)
+  }    
   
   //===============================================
-  // api.postAdv(createData.category, createData);  
+  api.postAdv(createData.category, createData);  
   //===============================================
 
   advForm.reset();
   clearImages(allImg);
   returnMarkToStart(allLabelArr);
-  removeInputFile(allPhotoInputs);  
+  removeInputFile(allPhotoInputs);
+  
+  console.log(createData)
 }
+//=================================
 function previewImg (event){
+ 
   if(event.target === event.currentTarget){
     return;
   }
+ 
+  changeImgBlock(event);
+  
   if (event.target.dataset.id) {
     const file = event.target.files[0];
 
     const inputID = event.target.dataset.id;
     const img = document.querySelector(`.input-label__img--${inputID}`);
     const reader = new FileReader();    
-
-    reader.onloadend = () => {
+   
+    reader.onloadend = () => {     
       img.src = reader.result;
       productImage = reader.result;
       img.setAttribute('data-img', productImage);
     };
 
     if (file) {
-      reader.readAsDataURL(file);
+      reader.readAsDataURL(file);      
     } else {
       img.src = '';
-    }
-  }
+    }      
+  }   
 }
 
+//======================
+
+function changeImgBlock (event){ 
+  const imgTarget = event.target;
+  imgTarget.nextElementSibling.classList.remove('choose-this');
+  let imgId = Number(event.target.dataset.id);
+  
+  imgId += 1;   
+  
+  if (imgId > 6) {
+    return;
+  }
+
+  const nextImg = document.querySelector(`[data-id="${imgId}"]`);
+  nextImg.dataset.active = true; 
+  nextImg.nextElementSibling.classList.add('choose-this'); 
+}
