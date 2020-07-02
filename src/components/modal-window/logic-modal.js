afe68a84b3c40cb952c4a5a8faae252bb3fb25e4
Y.Tern <ternxstyle@gmail.com>
2020-07-02 12:03:41 +0300

    change code_4

--- src/components/modal-window/logic-modal.js
+++ src/components/modal-window/logic-modal.js
@@ -1,11 +1,12 @@
 ////////////////////////////////////////////////////////////
 //LOGIC FOR MODAL WINDOW v.0.7//////////////////////////////
 ////////////////////////////////////////////////////////////
-​
+
 //////////////Вызвать метод modalBackdrop, передать разметку/////////////////////////////
 //<div class="modalContainer"></div> - этот див с классом должен быть в основной разметке,
 //так как на него повешены глобальные настройки
 /////////////////////////////////////////////////////////////////////////////////////////​
+
 export const modalBackDrop = innerElement => {
   const body = document.querySelector('body');
   const container = document.querySelector('.modalContainer');
@@ -14,13 +15,13 @@ export const modalBackDrop = innerElement => {
     return `
         ${innerElement}
     `;
-  };​
+  };
   const closeModal = () => {
     container.classList.remove('show-modal');
     container.addEventListener('click', close);
     document.removeEventListener('keydown', close);
     document.querySelector('body').style.overflow = 'unset';
-  };​
+  };
   const close = e => {
     if (
       e.target === document.querySelector('.modalContainer') ||
@@ -28,17 +29,17 @@ export const modalBackDrop = innerElement => {
     ) {
       closeModal();
     } else return;
-  };​
+  };
   container.innerHTML = createModalMarkup(closeModal);
   container.classList.add('show-modal', 'transition-effect');
   document.querySelector('body').style.overflow = 'hidden';
   container.addEventListener('click', close);
   document.addEventListener('keydown', close);
   return closeModal;
-};​
+};
 
 ////////Your element///////////////////////////////////////////////////////////////
-​
+
 /* const adv = () => {
   const innerElement2 = `
     <div class="innerBlock2">
